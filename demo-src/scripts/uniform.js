7311b5aa27ed8bbde235495be43e5a726ad485e5
Peter Szerzo <peter.szerzo@airtame.com>
2016-08-01 18:52:46 +0200

    Added multiple frozen demo

--- demo-src/scripts/uniform.js
+++ demo-src/scripts/uniform.js
@@ -1,20 +1,37 @@
-function generateDrops(n) {
-  const drops = [];
-  for (let i = 0; i < n; i++) {
-    drops.push(generateDrop());
-  }
-  return drops;
-}
-
-function generateDrop() {
+function generateDrop(isTop) {
+  const random = Math.random();
   return {
     x: Math.random(),
-    y: Math.random(),
+    y: isTop ? -.1 : Math.random(),
     scale: 1,
     blur: 0
   };
 }
 
+function generateInitialDrops(n) {
+  const drops = [];
+  for (let i = 0; i < n; i++) {
+    drops.push(generateDrop(false));
+  }
+  return drops;
+}
+
+function stepDrop(drop) {
+  if (drop.y > 1.1) {
+    return generateDrop(true);
+  }
+  return {
+    scale: drop.scale,
+    blur: drop.blur,
+    x: drop.x,
+    y: drop.y + .003
+  };
+}
+
+function stepDrops(drops) {
+  return drops.map(stepDrop);
+}
+
 export default () => {
-  cphRain(generateDrops(30), drops => drops, document.getElementById('app'));
+  cphRain(generateInitialDrops(30), stepDrops, document.getElementById('app'));
 }

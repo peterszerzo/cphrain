bb1307bee93a72d4beeaae3dd27e7ff9d03bcf0f
Peter Szerzo <peter.szerzo@airtame.com>
2016-08-02 12:27:43 +0200

    Add keyboard support

--- demo-src/index.js
+++ demo-src/index.js
@@ -1,3 +1,5 @@
+import domReady from 'domready';
+
 import rains from './rains.json';
 
 import './demo.css';
@@ -5,6 +7,24 @@ const cphRainDemo = {};
 
 rains.forEach((rain) => {
   cphRainDemo[rain] = require(`./scripts/${rain}.js`).default;
+});
+
+function addKeyboardListeners(document) {
+  document.addEventListener('keydown', (e) => {
+    const {keyCode} = e;
+    if (keyCode === 37) {
+      let node = document.getElementsByClassName('nav-link--left')[0];
+      node && node.click();
+    }
+    if (keyCode === 39) {
+      let node = document.getElementsByClassName('nav-link--right')[0];
+      node && node.click();
+    }
+  });
+}
+
+domReady(() => {
+  addKeyboardListeners(document);
 });
 
 global.cphRainDemo = cphRainDemo;
